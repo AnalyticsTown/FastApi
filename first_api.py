1bac99f7f7a9aab3b6d11e64f7e27b3491159bca
AnalyticsTown <102179354+AnalyticsTown@users.noreply.github.com>
2022-03-24 12:21:48 -0300

    Update first_api.py

--- first_api.py
+++ first_api.py
@@ -30,28 +30,25 @@ def registro(mail:str, password: str):
     print(response)
     
 @app.post("/login/{mail},{password}")
-def login(mail:str,password:str):
-    username= mail,
-    password=password
+def login(mail:str, password:str):
+   
+    #username= mail,
+    #contrasenia=password
     client = boto3.client('cognito-idp', region_name=os.getenv('COGNITO_REGION_NAME'))
     response = client.initiate_auth(
     ClientId = os.getenv('COGNITO_USER_CLIENT_ID'),
-    AuthFlow='USER_PASSWORD_AUTH',
+    AuthFlow ='USER_PASSWORD_AUTH',
     AuthParameters = {
-        'USERNAME': username,
+        'USERNAME': mail,
         'PASSWORD': password
     }
     )
     try:
         print (response)
-        print ("logueo exitoso")
+        at= response['AuthenticationResult']['AccessToken']
+        return ("logueo exitoso")
     except:
-        print("algo fallo")
-
-    at= response['AuthenticationResult']['AccessToken']
-
-
-
+        return ("algo fallo")
 
 
 
