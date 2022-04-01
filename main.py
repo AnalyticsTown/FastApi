dd43bc0a77ea2276170af1c3b0ae9d5652f3dbb4
AnalyticsTown <102179354+AnalyticsTown@users.noreply.github.com>
2022-04-01 09:22:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 allow_origins=['*']
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
