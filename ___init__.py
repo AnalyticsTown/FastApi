5fb8880b25561a08c4b546bfcf46049eb7dd7108
AnalyticsTown <102179354+AnalyticsTown@users.noreply.github.com>
2022-05-30 17:23:06 -0300

    Update ___init__.py

--- ___init__.py
+++ ___init__.py
@@ -1,12 +1,1 @@
-from fastapi import FastAPI
-from mangum import Mangum
 
-app = FastAPI()
-
-
-@app.get("/")
-def get_root():
-    return {"message": "FastAPI running in a Lambda function"}
-
-
-handler = Mangum(app)
